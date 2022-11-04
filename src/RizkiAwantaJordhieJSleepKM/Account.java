54ed03e06fc6b3607f29c35089624845b166fd03
Jordinia <rizki.awanta@ui.ac.id>
2022-11-04 22:40:45 +0700

    PT6 First Push

--- src/RizkiAwantaJordhieJSleepKM/Account.java
+++ src/RizkiAwantaJordhieJSleepKM/Account.java
@@ -1,33 +1,46 @@
 package RizkiAwantaJordhieJSleepKM;
 //package RizkiAwantaJordhieJSleepKM;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 /**
  * Account class contains account information.
  *
  * @author (Rizki Awanta Jordhie)
- * @version (4 - CS4 - 04/10/2022)
+ * @version (5 - PT6 - 04/11/2022)
  */
 public class Account extends Serializable{
+
     public String name;
     public String email;
     public String password;
+
+    public static final String REGEX_EMAIL = "^[A-Za-z0-9]+(?:\\\\.[\\w!#$%&’*+/=?`{|}~^]+)*@(?:[a-zA-Z]+\\.)+[a-zA-Z]{2,6}$";
+    public static final String REGEX_PASSWORD = "((?=\\S+$)(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z]).{8,})";
+
+
     /**
      * Constructor for objects of class Account
      */
     public Account(String name, String email, String password){
-        //super(id);
+        super();
         this.name = name;
         this.email = email;
         this.password = password;
     }
 
-    public Object write() {
-        return null;
+    public boolean validate(){
+        return this.email.matches(REGEX_EMAIL) && this.password.matches(REGEX_PASSWORD);
     }
-    public boolean read(String content){
-        return true;
-    }
+
+//    public Object write() {
+//        return null;
+//    }
+//
+//    public boolean read(String content){
+//        return true;
+//    }
 
     public String toString(){
         return "\nName= " + name + "\nEmail= " + email + "\nPassword= " + password;
--- src/RizkiAwantaJordhieJSleepKM/JSleep.java
+++ src/RizkiAwantaJordhieJSleepKM/JSleep.java
@@ -3,7 +3,7 @@ package RizkiAwantaJordhieJSleepKM;
  * JSleep main.
  *
  * @author (Rizki Awanta Jordhie)
- * @version (6 - CS6 - 01/11/2022)
+ * @version (7 - PT6 - 04/11/2022)
  */
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -14,81 +14,60 @@ import com.google.gson.*;
 
 public class JSleep
 {
-    public static void main(String[] args){
-        Renter testRegex = new Renter("Netlab_", "081234567890", "Jl Margonda Raya");
-        System.out.println(testRegex.validate());
-        try{
-            String filepath = "C:\\Users\\Rizky Awanta Jordhie\\Documents\\All-Files\\TUGAS\\UI\\2-Pembelajaran\\3-Kuliah\\Materi x Buku\\Semester 3\\Object Oriented Programming\\Praktikum OOP\\Code\\JSleep\\src\\RizkiAwantaJordhieJSleepKM\\json\\randomRoomList.json";
 
-            JsonTable<Room> tableRoom = new JsonTable<>(Room.class, filepath);
-            List<Room> filterTableRoom = filterByAccountId(tableRoom, 1, 0, 5);
-            filterTableRoom.forEach(room -> System.out.println(room.toString()));
+    public static void main(String[] args){
+//        Account testRegex = new Account("JOR","jordhie@gmail.com","Abcd12345");
+//        Account testRegexfalse = new Account("JOR FALSE"," jordhie@ui.ac.id","Abcd12345");
+//        System.out.println(testRegex.validate());
+//        System.out.println(testRegexfalse.validate());
+        try {
+            String filepath = "src\\RizkiAwantaJordhieJSleepKM\\json\\account.json";
+            JsonTable<Account> tableAccount = new JsonTable<Account>(Account.class, filepath);
+            Account account = new Account("name","email","password");
+            tableAccount.add(account);
+            tableAccount.writeJson();
+            System.out.print(tableAccount);
         }
-        catch (Throwable t){
+        catch (Throwable t) {
             t.printStackTrace();
         }
+        for (int i = 0; i < 10; i++){
+            ThreadingObject thread = new ThreadingObject("Thread " + i);
+        }
+        // CS6
+//        Renter testRegex = new Renter("Netlab_", "081234567890", "Jl Margonda Raya");
+//        System.out.println(testRegex.validate());
+//        try{
+//            String filepath = "C:\\Users\\Rizky Awanta Jordhie\\Documents\\All-Files\\TUGAS\\UI\\2-Pembelajaran\\3-Kuliah\\Materi x Buku\\Semester 3\\Object Oriented Programming\\Praktikum OOP\\Code\\JSleep\\src\\RizkiAwantaJordhieJSleepKM\\json\\randomRoomList.json";
+//
+//            JsonTable<Room> tableRoom = new JsonTable<>(Room.class, filepath);
+//            List<Room> filterTableRoom = filterByAccountId(tableRoom, 1, 0, 5);
+//            filterTableRoom.forEach(room -> System.out.println(room.toString()));
+//        }
+//        catch (Throwable t){
+//            t.printStackTrace();
     }
+
     public static Room createRoom(){
         Price price = new Price(100000,5);
         Room room = new Room(1, "Hotel", 30,price,Facility.AC,City.DEPOK,"Jalan Margonda Raya");
         return room;
     }
+
     public static List<Room> filterByCity(List<Room> list, String search, int page, int pageSize){
         return Algorithm.paginate(list, page, pageSize, room->room.city.toString().toLowerCase().contains(search.toLowerCase()));
     }
+
     public static List<Room> filterByPrice(List<Room> list, double minPrice, double maxPrice){
+        if (maxPrice == 0){
+            return Algorithm.<Room>collect(list, p -> (p.price.price >= minPrice));
+        }
         return Algorithm.<Room>collect(list, p -> (p.price.price <= maxPrice) && (p.price.price >= minPrice));
     }
+
     public static List<Room> filterByAccountId(List<Room> list, int accountId, int page, int pageSize){
         return Algorithm.paginate(list, page, pageSize, room->(room.accountId == accountId));
     }
-//    public static int getHotelId(){
-//        return 0;
-//    }
-//    public static String getHotelName(){
-//        return "hotel";
-//    }
-//    public static boolean isDiscount(){
-//        return true;
-//    }
-//    public static float getDiscountPercentage(int beforeDiscount, int afterDiscount){
-//        if(beforeDiscount <= afterDiscount){
-//            return 0;
-//        }
-//        else {
-//            float diff = beforeDiscount - afterDiscount;
-//            return (diff/beforeDiscount)*100;
-//        }
-//    }
-//    public static int getDiscountedPrice(int price, float discountPercentage){
-//        if(discountPercentage >= 100.0f){
-//            discountPercentage = 100.0f;
-//        }
-//        return (int) (price - (price*(discountPercentage/100.0f)));
-//    }
-//    public static int getOriginalPrice(int discountedPrice, float discountPercentage){
-//        if(discountedPrice <= 0){
-//            return 0;
-//        }
-//        float orgPrice = (100/(100-discountPercentage))*discountedPrice;
-//        return (int) orgPrice;
-//    }
-//    public static float getAdminFeePercentage(){
-//        return 0.05f;
-//    }
-//    public static int getAdminFee(int price){
-//        return (int) (price * getAdminFeePercentage());
-//    }
-//    public static int getTotalPrice(int price, int numberOfNight){
-//        return  ((price * numberOfNight) + getAdminFee(price*numberOfNight));
-//    }
-//    class Country{
-//        public String name;
-//        public int population;
-//        public List<String> listOfStates;
-//    }
-
-
 }
 
 
